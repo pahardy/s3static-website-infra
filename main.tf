928cc1c1d52d9e9ce6a5a604605d05acf61245a7
Administrator <gitlab_admin_29fbfc@example.com>
2025-03-10 21:34:51 +0000

    Update file main.tf

--- main.tf
+++ main.tf
@@ -28,59 +28,49 @@ resource "aws_s3_bucket_website_configuration" "s3_bucket_static_config" {
   }
 }
 
-
 data "aws_acm_certificate" "acm_cert" {
   domain   = "pahardy.com" # Replace with your domain
   statuses = ["ISSUED"]
   most_recent = true
 }
 
-resource "aws_cloudfront_distribution" "s3_static_distribution" {
+# Create CloudFront Distribution
+resource "aws_cloudfront_distribution" "s3_distribution" {
   origin {
     domain_name              = aws_s3_bucket.static-s3-site.bucket_regional_domain_name
-    origin_id                = "myS3origin"
+    origin_id                = "s3-origin"
   }
-
 
   enabled             = true
-  is_ipv6_enabled     = true
-  comment             = "CloudFront distribution for static website hosted in S3"
   default_root_object = "index.html"
-  aliases = ["sdelements.pahardy.com"]
-  price_class = "PriceClass_100"
 
-  restrictions {
-    geo_restriction {
-      restriction_type = "whitelist"
-      locations        = ["US", "CA", "GB", "DE"]
-    }
-  }
-  
+  # Default cache behavior
   default_cache_behavior {
-    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    allowed_methods  = ["GET", "HEAD"]
     cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "myS3origin"
+    target_origin_id = "s3-origin"
+
+    viewer_protocol_policy = "redirect-to-https"
 
     forwarded_values {
       query_string = false
-
       cookies {
         forward = "none"
       }
     }
-
-  logging_config {
-    include_cookies = false
-    bucket          = aws_s3_bucket.static-s3-site
-    prefix          = "myprefix"
   }
 
-  # Use the ACM certificate for HTTPS
+  # Price class (Use PriceClass_All for global coverage)
+  price_class = "PriceClass_100"
+
+  # Use an SSL certificate from ACM (Optional)
   viewer_certificate {
-    acm_certificate_arn      = data.aws_acm_certificate.acm_cert.arn
-    ssl_support_method       = "sni-only"
-    minimum_protocol_version = "TLSv1.2_2021"
+    acm_certificate_arn = data.aws_acm_certificate.acm_cert
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
   }
 }
-}
-
