5ba1c7210d811e6835d2a87b0659185286ed4383
Administrator <gitlab_admin_29fbfc@example.com>
2025-03-11 13:01:22 +0000

    Update file main.tf

--- main.tf
+++ main.tf
@@ -21,19 +21,53 @@ resource "aws_s3_bucket" "static-s3-site" {
   }
 }
 
-#Create a CloudFront Origin Access Control configuration
-#resource "aws_cloudfront_origin_access_control" "s3_static_cloudfront_oac" {
-#  name                              = "s3_static_oac"
-#  description                       = "Policy for CloudFront with S3 source"
-#  origin_access_control_origin_type = "s3"
-#  signing_behavior                  = "always"
-#  signing_protocol                  = "sigv4"
-#}
+Create a CloudFront Origin Access Control configuration
+resource "aws_cloudfront_origin_access_control" "s3_static_cloudfront_oac" {
+  name                              = "s3_static_oac"
+  description                       = "Policy for CloudFront with S3 source"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
 
-#Create a CloudFront distribution
-#resource "aws_cloudfront_distribution" "s3_distribution" {
-#  origin {
-#    domain_name              = aws_s3_bucket.static-s3-site.bucket_regional_domain_name
-#    origin_access_control_id = aws_cloudfront_origin_access_control.default.id
-#    origin_id                = local.s3_origin_id
-#  }
+Create a CloudFront distribution
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name              = aws_s3_bucket.static-s3-site.bucket_regional_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.s3_static_cloudfront_oac.id
+    origin_id                = "myOriginId"
+  }
+
+  enabled = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+  
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = ["US", "CA", "GB", "DE"]
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
