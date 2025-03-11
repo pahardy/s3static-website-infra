b3d6cdc3391408b1a9807c5c91b09821f32148d8
Administrator <gitlab_admin_29fbfc@example.com>
2025-03-11 12:43:05 +0000

    Update file main.tf

--- main.tf
+++ main.tf
@@ -21,4 +21,19 @@ resource "aws_s3_bucket" "static-s3-site" {
   }
 }
 
+#Create a CloudFront Origin Access Control configuration
+#resource "aws_cloudfront_origin_access_control" "s3_static_cloudfront_oac" {
+#  name                              = "s3_static_oac"
+#  description                       = "Policy for CloudFront with S3 source"
+#  origin_access_control_origin_type = "s3"
+#  signing_behavior                  = "always"
+#  signing_protocol                  = "sigv4"
+#}
+
 #Create a CloudFront distribution
+#resource "aws_cloudfront_distribution" "s3_distribution" {
+#  origin {
+#    domain_name              = aws_s3_bucket.static-s3-site.bucket_regional_domain_name
+#    origin_access_control_id = aws_cloudfront_origin_access_control.default.id
+#    origin_id                = local.s3_origin_id
+#  }
